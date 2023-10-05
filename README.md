2d83c81e5ecab2938c731ab1fd8f6e2c23893f8e
Steve Smodish <smodish@hotmail.com>
2023-10-05 10:59:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vite_React_Tailwind_Barebones
+Vite React Tailwind and NOTHING else!
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="text-4xl">Vite and Tailwind</div>
+    </>
+  );
+}
+
+export default App;
